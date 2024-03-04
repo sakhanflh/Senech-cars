512ce8b5b2e85799b4b1fc121a58ed32d6854d3b
Sakha_Naufal_Huda <sakhanaufal2@gmail.com>
2024-03-05 05:19:28 +0700

    third push

--- src/components/utils/navbar/Leftbar.jsx
+++ src/components/utils/navbar/Leftbar.jsx
@@ -1,31 +1,47 @@
+import { useState } from "react";
 import { MdKeyboardArrowDown } from "react-icons/md";
 
 export function Leftbar({onShowLeftbar}){
+    const [showScroll, setShowScroll] = useState(false)
+
+    function handleShowScroll(){
+        setShowScroll(!showScroll)
+    }
+
     return(
         <>
         <div className={onShowLeftbar ? "absolute bg-white w-60 h-[38rem] translate-y-[335px] -translate-x-3 transition-all duration-500" : "absolute bg-red-50 w-60 h-[38rem] translate-y-[335px] -translate-x-96 transition-all duration-500"}>
-            <ul className="py-5">
-                <div className="border-l-8 border-dark w-full h-10 items-center flex">
-                    <li className="px-5 text-lg">Home</li>
+            <ul className="py-5 text-primary text-xl">
+                <div className="border-l-8 border-dark w-full h-10 items-center flex ">
+                    <li className="px-5 text-xl font-semibold">Home</li>
                 </div>
 
                 <div className="border-l-4 w-full h-10 items-center flex">
-                    <li className="px-5 text-lg">Buy</li>
+                    <li className="px-5">Buy</li>
                 </div>
 
                 <div className="border-l-4 w-full h-10 items-center flex">
-                    <li className="px-5 text-lg">How it works</li>
+                    <li className="px-5">How it works</li>
                 </div>
 
                 <div className="border-l-4 w-full h-10 items-center flex">
-                    <li className="px-5 text-lg">Review</li>
+                    <li className="px-5 ">Review</li>
                 </div>
 
-                <div className="border-l-4 w-full h-10 items-center flex">
-                    <li className="flex items-center px-5 text-lg">Services <MdKeyboardArrowDown/></li>
+                <div className="border-l-4 w-full h-10 items-center flex ">
+                    <li className="flex items-center px-5" onClick={handleShowScroll}>Services <MdKeyboardArrowDown/></li>
                 </div>
-                
             </ul>
+                <div className="w-full h-48 px-5">
+                    <div className={showScroll ? "w-full h-full transition-all duration-500 p-5 bg-slate-100 rounded-lg" : "w-full h-0 p-5 transition-all duration-500 bg-slate-100 rounded-lg"}>
+                        <ul className="flex flex-col justify-between h-full text-primary text-lg">
+                            <li>CarAudit</li>
+                            <li>Delivery</li>
+                            <li>Financing</li>
+                            <li>Warranty</li>
+                        </ul>
+                    </div>
+                </div>
         </div>
         </>
     )
