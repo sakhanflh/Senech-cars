5407a0be0f22fdfe3f449c73c8bac297f9b98cc9
Sakha_Naufal_Huda <sakhanaufal2@gmail.com>
2024-03-06 13:29:22 +0700

    still error

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -5,7 +5,7 @@ import Hometwo from "../components/utils/home/homeSectionTwo";
 export default function HomePage(){
     return(
         <>
-            <div className="w-full">
+            <div className="w-full h-[500vh]">
                 <Header />
                 <Headerm />
                 <Homeone/>
