0289096a050b62165738251cf79c0605386b9ea5
Sakha_Naufal_Huda <sakhanaufal2@gmail.com>
2024-03-08 03:40:37 +0700

    slider but not finish

--- src/components/utils/home/homeSwiperMobile.jsx
+++ src/components/utils/home/homeSwiperMobile.jsx
@@ -4,16 +4,16 @@ import 'swiper/css';
 export default function HomeSwiperMobile(){
     return(
         <Swiper
-            spaceBetween={10}
+            spaceBetween={5}
             slidesPerView={3}
             onSlideChange={() => console.log('slide change')}
             onSwiper={(swiper) => console.log(swiper)}
         >
-            <SwiperSlide><img src="/image/CityCar.jpg" alt="" /></SwiperSlide>
-            <SwiperSlide><img src="/image/EstateCar.jpg" alt="" /></SwiperSlide>
-            <SwiperSlide><img src="/image/FamilyCar.jpg" alt="" /></SwiperSlide>
-            <SwiperSlide><img src="/image/SuvCar.jpg" alt="" /></SwiperSlide>
-            <SwiperSlide><img src="/image/SportCar.jpg" alt="" /></SwiperSlide>
+            <SwiperSlide><img src="/image/CityCar.jpg" alt="" style={{width: 'auto', height: '120px'}}/></SwiperSlide>
+            <SwiperSlide><img src="/image/EstateCar.jpg" alt="" style={{width: 'auto', height: '120px'}}/></SwiperSlide>
+            <SwiperSlide><img src="/image/FamilyCar.jpg" alt="" style={{width: 'auto', height: '120px'}}/></SwiperSlide>
+            <SwiperSlide><img src="/image/SuvCar.jpg" alt="" style={{width: 'auto', height: '120px'}}/></SwiperSlide>
+            <SwiperSlide><img src="/image/SportCar.jpg" alt="" style={{width: 'auto', height: '120px'}}/></SwiperSlide>
         </Swiper>
     )
 }
