15bbebf3f6713f28de3ab5804ec6ab79fd10cde7
Sakha_Naufal_Huda <sakhanaufal2@gmail.com>
2024-03-09 23:06:34 +0700

    add icon on sidebar

--- src/components/utils/navbar/Leftbar.jsx
+++ src/components/utils/navbar/Leftbar.jsx
@@ -1,5 +1,9 @@
 import { useState } from "react";
-import { MdKeyboardArrowDown } from "react-icons/md";
+import { MdHome, MdKeyboardArrowDown } from "react-icons/md";
+import { MdOutlineToggleOff } from "react-icons/md";
+import { GiTakeMyMoney } from "react-icons/gi";
+import { FaPenToSquare, FaUserGear } from "react-icons/fa6";
+import { PiShieldStarFill } from "react-icons/pi";
 
 export function Leftbar({onShowLeftbar}){
     const [showScroll, setShowScroll] = useState(false)
@@ -11,25 +15,25 @@ export function Leftbar({onShowLeftbar}){
     return(
         <>
         <div className={onShowLeftbar ? "z-50 absolute bg-white  w-60 h-[47rem] translate-y-[407px] -translate-x-3 transition-all duration-500" : "absolute bg-white w-60 h-[47rem] translate-y-[407px] -translate-x-96 transition-all duration-500 z-50"}>
-            <ul className="py-5 text-primary text-xl">
+            <ul className="py-5 text-primary text-xl  flex flex-col gap-5">
                 <div className="border-l-8 border-dark w-full h-10 items-center flex ">
-                    <li className="px-5 text-xl font-semibold">Home</li>
+                    <li className="px-5 text-xl font-semibold flex items-center gap-2"><MdHome/> Home</li>
                 </div>
 
                 <div className="border-l-4 w-full h-10 items-center flex">
-                    <li className="px-5">Buy</li>
+                    <li className="px-5 items-center flex gap-2"><GiTakeMyMoney/> Buy</li>
                 </div>
 
                 <div className="border-l-4 w-full h-10 items-center flex">
-                    <li className="px-5">How it works</li>
+                    <li className="px-5 items-center flex gap-2"><FaUserGear/> How it works</li>
                 </div>
 
                 <div className="border-l-4 w-full h-10 items-center flex">
-                    <li className="px-5 ">Review</li>
+                    <li className="px-5 items-center flex gap-2"><PiShieldStarFill/> Review</li>
                 </div>
 
                 <div className="border-l-4 w-full h-10 items-center flex ">
-                    <li className="flex items-center px-5" onClick={handleShowScroll}>Services <MdKeyboardArrowDown/></li>
+                    <li className="flex items-center px-5 gap-2" onClick={handleShowScroll}><FaPenToSquare/> Services <MdKeyboardArrowDown/></li>
                 </div>
             </ul>
                 <div className="w-full h-48 px-5">
@@ -42,6 +46,15 @@ export function Leftbar({onShowLeftbar}){
                         </ul>
                     </div>
                 </div>
+                
+            <div className=" border-t-2  gap-5 w-full h-full p-5 text-lg text-primary flex flex-col bg-slate-100 ">
+                <div className="flex items-center py-4 justify-around w-full ">
+                    <p className="text-xl">Dark mode</p>
+                    <MdOutlineToggleOff className="text-primary text-3xl"/>
+                </div>
+
+                <button className="bg-secondary text-white py-3 w-full justify-center rounded-lg flex items-center">Login</button>
+            </div>
         </div>
         </>
     )
