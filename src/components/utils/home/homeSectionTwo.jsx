d0cd0b19a64a1586731a409dac42c2052e3a44d8
Sakha_Naufal_Huda <sakhanaufal2@gmail.com>
2024-03-09 22:21:24 +0700

    delete bg color

--- src/components/utils/home/homeSectionTwo.jsx
+++ src/components/utils/home/homeSectionTwo.jsx
@@ -3,11 +3,11 @@ import HomeSwiperMobile from "./homeSwiperMobile";
 export default function Hometwo(){
     return(
         <>
-        <div className="w-full pt-80 bg-red-500">
+        <div className="w-full pt-80 bg-white">
             <div>
                 <h1 className="text-2xl font-bold px-5">Popular at Senech Cars</h1>
             </div>
-            <div className="bg-green-500 w-full h-32">
+            <div className="bg-white w-full h-32">
                 <HomeSwiperMobile/>
             </div>
         </div>
--- src/components/utils/login/LoginMobile.jsx
+++ src/components/utils/login/LoginMobile.jsx
@@ -5,7 +5,7 @@ import { FaFacebook } from "react-icons/fa";
 export default function LoginMobilePage({onShowLogin}){
     return(
         <>
-        <div className={onShowLogin ? "w-full h-[28rem] translate-y-0 flex items-end absolute text-dark left-0 top-[23rem] transition-all duration-500" : "left-0 top-[23rem] w-full h-[28rem] translate-y-full transition-all duration-500 items-end bg-green-500 absolute text-dark"}>
+        <div className={onShowLogin ? "w-full h-[28rem] translate-y-0 opacity-100 flex items-end absolute text-dark left-0 top-[23rem] transition-all duration-500" : "left-0 top-[23rem] opacity-0 w-full h-[28rem] translate-y-full transition-all duration-500 items-end bg-green-500 absolute text-dark"}>
             <div className=" w-full bg-white overflow-hidden rounded-t-2xl h-[28rem] justify-between flex flex-col">
                 <div className="bg-slate-200 px-5 pt-5 h-[40%] flex flex-col gap-4">
                     <h1 className="text-xl font-semibold">Welcome</h1>
